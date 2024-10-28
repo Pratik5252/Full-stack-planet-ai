c5980d229f6f3842c0e9ed73d5d9a92898de9d9b
Pratik Yesane <pratiky2000@gmail.com>
2024-10-28 18:56:09 +0530

    Added cors

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from .api.routes import router
-from .db.database import metadata,database,engine
+from .db.database import metadata, database, engine
+
+# adding cors header
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
+
 
 @app.get("/")
 async def root():
@@ -17,9 +30,8 @@ async def startup():
     await database.connect()
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
-    
+
+
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-      
